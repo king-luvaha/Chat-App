c17db2bf53397c764ec3295570598a8fe88efeb0
Ezra Luvaha <mrluvaha@gmail.com>
2024-05-05 07:18:32 +0300

    3rd commit

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,8 @@ const registerUser = require("../controller/registerUser")
 const checkEmail = require("../controller/checkEmail")
 const checkPassword = require("../controller/checkPassword")
 const userDetails = require("../controller/userDetails")
+const logout = require("../controller/logout")
+const updateUserDetails = require("../controller/updateUserDetails")
 
 const router = express.Router()
 
@@ -17,5 +19,11 @@ router.post('/password',checkPassword)
 
 //login user details
 router.get('/user-details',userDetails)
+
+//logout
+router.get('/logout',logout)
+
+//update user details
+router.post('/update-user',updateUserDetails)
 
 module.exports = router
