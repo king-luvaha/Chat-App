fba480fbd777ba09f862ee219d3cb9196b66e6d7
Ezra Luvaha <mrluvaha@gmail.com>
2024-05-09 18:57:11 +0300

    17th commit

--- server/index.js
+++ server/index.js
@@ -6,15 +6,17 @@ const router = require('./routes/index');
 const cookiesParser = require('cookie-parser');
 const { app, server } = require('./socket/index')
 
-// const app = express()
+const PORT = process.env.PORT || 8080
+
+const app = express();
+
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true
 }));
+
 app.use(express.json())
 app.use(cookiesParser())
-
-const PORT = process.env.PORT || 8080
 
 app.get('/',(request, response)=>{
     response.json({
